4200d850b1a9f6392b5a19fd5dfc85bf51af1637
mrbruce516 <46187947+mrbruce516@users.noreply.github.com>
2024-08-28 18:26:00 +0800

    更新

--- javScrapy/shell/download.sh
+++ javScrapy/shell/download.sh
@@ -1,4 +1,4 @@
-cat /data/scrapy/scrapyd/data/`date +%Y-%m-%d`.magnet.txt | xargs -n 1 -P 5 -I {} curl -s -X POST \
+cat /data/scrapy/scrapyd/scrapyd/data/`date +%Y-%m-%d`.magnet.txt | xargs -n 1 -P 5 -I {} curl -s -X POST \
     --header 'Content-Type: application/json' \
     --data '{"jsonrpc": "2.0", "method": "aria2.addUri", "id": "1", "params": ["token:mrbruce516",["{}"],{"dir":"/downloads/av"}]}' \
     http://127.0.0.1:6800/jsonrpc
